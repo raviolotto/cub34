d6b254aa2478c931e01474ddc6deaac46bad09ae
mcamilli <mcamilli@student.42roma.it>
2024-05-09 12:37:53 +0200

    little mod

--- ray/ray2.c
+++ ray/ray2.c
@@ -6,7 +6,7 @@
 /*   By: mcamilli <mcamilli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 14:14:40 by mcamilli          #+#    #+#             */
-/*   Updated: 2024/05/03 21:42:59 by mcamilli         ###   ########.fr       */
+/*   Updated: 2024/05/09 12:37:17 by mcamilli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,18 @@
 void ft_fakeray(t_data *data, int i)
 {	
 	int p;
+	int i;
 	
 	p = ((623.25 / data->ray_lenght) / 2.0);
-	while (p >= 0)
+	if (p > 225)
+		p = 225;
+	while (p > 0)
 	{
 		mlx_pixel_put(data->mini.mlx, data->mini.mlx_win, i, 225 - p, 0xFF0000 );
-		mlx_pixel_put(data->mini.mlx, data->mini.mlx_win, i, 226 + p, 0xFF0000 );
+		mlx_pixel_put(data->mini.mlx, data->mini.mlx_win, i, 225 + p, 0xFF0000 );
 		p--;
 	}
+	mlx_pixel_put(data->mini.mlx, data->mini.mlx_win, i, 225, 0xFF0000 );
+	
 	//printf("retta n%d, ray lenght %f, ray_x %f, ray_y %f, mov_ang %f\n", i, data->ray_lenght, data->ray_x, data->ray_y, data->player.mov_ang);
 }
