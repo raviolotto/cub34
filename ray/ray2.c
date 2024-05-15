d8a3e800af54cf0b32e42b01226b29d06d2e6b48
mcamilli <mcamilli@student.42roma.it>
2024-05-15 12:16:26 +0200

    snellire raycast

--- ray/ray2.c
+++ ray/ray2.c
@@ -6,7 +6,7 @@
 /*   By: mcamilli <mcamilli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 14:14:40 by mcamilli          #+#    #+#             */
-/*   Updated: 2024/05/14 19:02:50 by mcamilli         ###   ########.fr       */
+/*   Updated: 2024/05/15 12:12:05 by mcamilli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ void ft_floor_n_ceil(t_data *data, int col)
 	}
 }
 
-/*
+
 void ft_fakeray(t_data *data, int i)
 {
 	int p;
@@ -73,7 +73,7 @@ void ft_fakeray(t_data *data, int i)
 	my_mlx_pixel_put(data, i, 225, create_trgb(0, 020, 100, 100));
 	// printf("===================================retta n%d, ray lenght %f, ray_x %f, ray_y %f, mov_ang %f\n", i, data->ray_lenght, data->ray_x, data->ray_y, data->player.mov_ang);
 }
-*/
+
 int get_pixel(t_data *data, int x, int y)
 {
 	int *dst;
@@ -130,32 +130,32 @@ void ft_fakeray(t_data *data, int i)
 }
 */
 
-void ft_fakeray(t_data *data, int i)
-{
-	int p;
-	double point;
-	int index;
+// void ft_fakeray(t_data *data, int i)
+// {
+// 	int p;
+// 	double point;
+// 	int index;
 
-	p = (623.25 / fabs(data->ray_lenght));
-	if (p > 225)
-		p = 225;
-	ft_floor_n_ceil(data, i);
-	if (data->player.AB_12 == 1)
-		point = data->ray_y - floor(data->player.dir_y);
-	else
-		point = data->ray_x - floor(data->player.dir_x);
-	int x = (int)round(point / 0.015625);
-	double image_part = (p * 1.0) / 64;
-	//printf("point = %f\n", point);
-	//double ptwo = 623.25 / fabs(data->ray_lenght);
-	int pzero = 225 - p/2;
-	while (p > 0)
-	{
-		//printf("y = %d\n", (int)round(ptwo * ((225 - p) - pzero) / 64));
-		//printf("y = %f\n", (((225 + p) - pzero) / image_part));
-		index = get_pixel(data, x, (int)round(p / image_part));
-		my_mlx_pixel_put(data, i, p + pzero, index);
-		p--;
-	}
-	// printf("===================================retta n%d, ray lenght %f, ray_x %f, ray_y %f, mov_ang %f\n", i, data->ray_lenght, data->ray_x, data->ray_y, data->player.mov_ang);
-}
+// 	p = (623.25 / fabs(data->ray_lenght));
+// 	if (p > 225)
+// 		p = 225;
+// 	ft_floor_n_ceil(data, i);
+// 	if (data->player.AB_12 == 1)
+// 		point = data->ray_y - floor(data->player.dir_y);
+// 	else
+// 		point = data->ray_x - floor(data->player.dir_x);
+// 	int x = (int)round(point / 0.015625);
+// 	double image_part = (p * 1.0) / 64;
+// 	//printf("point = %f\n", point);
+// 	//double ptwo = 623.25 / fabs(data->ray_lenght);
+// 	int pzero = 225 - p/2;
+// 	while (p > 0)
+// 	{
+// 		//printf("y = %d\n", (int)round(ptwo * ((225 - p) - pzero) / 64));
+// 		//printf("y = %f\n", (((225 + p) - pzero) / image_part));
+// 		index = get_pixel(data, x, (int)round(p / image_part));
+// 		my_mlx_pixel_put(data, i, p + pzero, index);
+// 		p--;
+// 	}
+// 	// printf("===================================retta n%d, ray lenght %f, ray_x %f, ray_y %f, mov_ang %f\n", i, data->ray_lenght, data->ray_x, data->ray_y, data->player.mov_ang);
+// }
--- src/hooknmove.c
+++ src/hooknmove.c
@@ -6,7 +6,7 @@
 /*   By: mcamilli <mcamilli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 13:23:32 by mcamilli          #+#    #+#             */
-/*   Updated: 2024/05/14 20:04:27 by mcamilli         ###   ########.fr       */
+/*   Updated: 2024/05/15 12:08:41 by mcamilli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,11 @@ int	controls_working(int command, t_data *data)
 
 	if (command == 115 || command == 65364) // indietro
 	{
-		double m;
-		double q;
-		data->rad_p = (data->player.mov_ang * CONST_RAD + (CONST_RAD / 2.0)) - 1.0471975511;
-		m = -(1/tan(data->rad_p));
-		q = data->player.pos_y - (m * data->player.pos_x);
+		// double m;
+		// double q;
+		// data->rad_p = (data->player.mov_ang * CONST_RAD + (CONST_RAD / 2.0)) - 1.0471975511;
+		// m = -(1/tan(data->rad_p));
+		// q = data->player.pos_y - (m * data->player.pos_x);
 		// data->prova_tex = mlx_xpm_file_to_image(data->mini.mlx, "east_texture.xpm", x, y);
 		//mlx_put_image_to_window(data->mini.mlx, data->mini.mlx_win, data->prova_E, 1, 1);
 	}
